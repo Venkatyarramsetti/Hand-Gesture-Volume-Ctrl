3b79dbf232e022ee3845e48424961c1dca902f33
Venkatyarramsetti <venkatyarramsetti33@gmail.com>
2025-06-07 17:51:39 +0530

    latest commit

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:8080"],
+  origin: [
+    "http://localhost:3000", 
+    "http://localhost:8080", 
+    "https://volumectrlbyhand.vercel.app"
+  ],
 }));
 
 
